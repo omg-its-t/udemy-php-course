f724960e41ba331ded5212d5f0518db632d5031d
Theo Mellske <blackvq35@gmail.com>
2019-12-05 21:32:22 -0600

    added methods to database class

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -35,4 +35,56 @@ class Database{
             echo $this->error;
         }
     }
+
+    //STEP 1 prepare statement with query
+    public function query($sql){
+         $this->stmt = $this->dbh->prepare($sql);
+    }
+
+    //STEP 2 bind values
+    public function bind($param, $value, $type = null){
+        //check to see what type it is
+        if(is_null($type)){
+            switch(true){
+                //if int set to int
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                //if bool set to bool
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                //if null set to null
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+        $this->stmt->bindValue($param, $value, $type);
+    }
+
+    //STEP 3 execute prepared statement
+    public function execute(){
+        return $this->stmt->execute();
+    }
+
+    // get multiple records from DB as an array of objects
+    public function resultSet(){
+        $this->execute();
+                                    // this make it return objects and not arrays                     
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    //get single record as object
+    public function single(){
+        $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+    }
+
+    //get row count
+    public function rowCount(){
+        return $this->stmt->rowCount();
+    }
 }
